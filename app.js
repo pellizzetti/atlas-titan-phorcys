96ff03ce9809656b512dc30d2ae2dec6c3d52eea
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2019-02-05 10:41:04 -0200

    Ajusta estrutura inicial do projeto

---
+++ app.js
@@ -0,0 +1,6 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.json({ healthy: true }));
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,11 @@
+﻿const request = require('supertest');
+const app = require('./app');
+
+describe('Test the root path', () => {
+  test('It should 200 and be healthy', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.healthy).toBe(true);
+  });
+});
